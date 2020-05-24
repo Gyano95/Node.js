a8ea363154d2815441ec08a24b7649768760789b
toba101 <toba.obiwale@outlook.com>
2020-05-24 17:17:53 +0100

    week 05

---
+++ assignments/week-05/note.js
@@ -0,0 +1,9 @@
+// Try, Catch and Finally //
+function imaginarySquareRoot(number) {
+    'use strict';
+    try {
+        return String(squareRoot(number));
+    } catch(error) {
+        return squareRoot(-number)+'i';
+    }
+}
